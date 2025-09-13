102b87a504a34a614d484de683a7d05b154061f4
khantMinHtut <khantminhtut009@gmail.com>
2025-09-13 21:08:23 +0700

    final commit

--- index.ts
+++ index.ts
@@ -7,9 +7,27 @@ app.use(express.json());
 
 app.get("/users", async (_, res) => {
   const users = await prisma.user.findMany({
-    where: { isMarried: false },
+    where: { isMarried: true, age: { gt: 30 } },
   });
   res.json(users);
+});
+
+app.put("/users", async (_, res) => {
+  const updatedUser = await prisma.user.update({
+    where: { email: "keo@example.com" },
+    data: {
+      age: 20,
+      nationality: "Myanmar",
+    },
+  });
+  res.json(updatedUser);
+});
+
+app.delete("/users", async (_, res) => {
+  const deletedUser = await prisma.user.delete({
+    where: { email: "sakura.t@example.com" },
+  });
+  res.json(deletedUser);
 });
 
 app.listen(4000, () => {
